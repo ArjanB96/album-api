49ca59be0a0f53c050361d76929736ee244e0fda
Flatn3ck <74960799+Flatn3ck@users.noreply.github.com>
2022-05-20 15:49:28 +0200

    initial

---
+++ Album-Api/Album.Api/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Album.Api.Controllers
+{
+    [ApiController]
+    //optional queryparameter name
+    [Route("[/api/hello/{name}]")]
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
