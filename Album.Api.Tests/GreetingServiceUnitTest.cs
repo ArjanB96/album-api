74c03f6c8c78b1cd05a484a571e1eda2aafd857d
Flatn3ck <74960799+Flatn3ck@users.noreply.github.com>
2022-06-01 19:44:16 +0200

    added first unit test

--- Album.Api.Tests/GreetingServiceUnitTest.cs
+++ Album.Api.Tests/GreetingServiceUnitTest.cs
@@ -1,15 +1,11 @@
 using System;
 using Xunit;
-using Album.Api;
+using Album.Api.Services;
 
 namespace Album.Api.Tests
 {
     public class GreetingServiceUnitTest
     {
-        Api.GreetingService.GreetingService greetingservice;
-
-        var test = new Api.GreetingService.GreetingService().Greet("test");
-
         [Fact]
         public void Greet_With_Correct_Name_Should_Greet_Name()
         {
@@ -17,11 +13,10 @@ namespace Album.Api.Tests
             string name = "Arjan";
 
             //Act
-            bool isValid = Greet(name);
-            string greeting = new GreetingService().Greet(name);
+            string greeting = GreetingService.Greet(name);
 
             //Assert
-            Assert.True(isValid, $"The name {name} is not valid");
+            Assert.Equal("Arjan", greeting, $"The name {name} is not valid");
         }
     }
 }
--- Album.Api/Services/GreetingService.cs
+++ Album.Api/Services/GreetingService.cs
@@ -1,19 +1,15 @@
-namespace Album.Api
+using System;
+
+namespace Album.Api.Services
 {
     public class GreetingService
     {
-        public GreetingService() { }
-        }
         public string Greet(string name)
         {
             if (name == "" || name == " " || name == null)
-            {
                 return "Hello World!";
-            }
             else
-            {
                 return $"Hello {name}!";
-            }
         }
     }
 }
