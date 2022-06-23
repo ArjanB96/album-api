67b39ab494a222ac0e5e37475cb3f874b12fb94d
Flatn3ck <74960799+Flatn3ck@users.noreply.github.com>
2022-06-23 15:48:30 +0200

    Update Startup.cs

--- Album.Api/Startup.cs
+++ Album.Api/Startup.cs
@@ -48,9 +48,10 @@ namespace Album.Api
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseCors(policy => policy
-                .WithOrigins("http://cnsd-react-app-4742-7680-6263.s3-website-us-east-1.amazonaws.com/)
+                .WithOrigins("http://cnsd-react-app-4742-7680-6263.s3-website-us-east-1.amazonaws.com")
                 .AllowAnyMethod()
-                .AllowAnyHeader()); 
+                .AllowAnyHeader());
+     
                 
             if (env.IsDevelopment())
             {
